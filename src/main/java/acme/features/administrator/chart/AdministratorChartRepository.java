50eedb8b1585c7d455b53de9e79c6a441ecaa321
Parras <Parras@LAPTOP-371PRH8M>
2019-12-18 23:28:37 +0100

    FINALIZANDO

--- src/main/java/acme/features/administrator/chart/AdministratorChartRepository.java
+++ src/main/java/acme/features/administrator/chart/AdministratorChartRepository.java
@@ -23,12 +23,12 @@ public interface AdministratorChartRepository extends AbstractRepository {
 	@Query("select a.status,count(a) FROM Application a group by a.status order by a.status")
 	Object[] findApplicationStatus();
 
-	@Query("select date(a.moment),count(a) FROM Application a where a.moment > ?1 and a.status = 0 group by a.moment")
+	@Query("select date(a.moment),count(a) FROM Application a where a.moment > ?1 and a.status = 0 group by day(a.moment)")
 	Object[] findRejectedApplicationsLastFourWeeks(Date d);
 
-	@Query("select date(a.moment),count(a) FROM Application a where a.moment > ?1 and a.status = 1 group by a.moment")
+	@Query("select date(a.moment),count(a) FROM Application a where a.moment > ?1 and a.status = 1 group by day(a.moment)")
 	Object[] findPendingApplicationsLastFourWeeks(Date d);
 
-	@Query("select date(a.moment),count(a) FROM Application a where a.moment > ?1 and a.status = 2 group by a.moment")
+	@Query("select date(a.moment),count(a) FROM Application a where a.moment > ?1 and a.status = 2 group by day(a.moment)")
 	Object[] findAcceptedApplicationsLastFourWeeks(Date d);
 }
