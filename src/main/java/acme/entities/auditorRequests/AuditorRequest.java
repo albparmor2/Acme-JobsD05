633d9fb7b0a36353d0cc1cc8d3f1f11fadf9c03c
Emilio Quintero <Emilio Quintero@DESKTOP-V2N42CJ.technicolor.net>
2019-12-18 20:34:54 +0100

    Pequeños cambios al model.uxf y a AuditorRequest

--- src/main/java/acme/entities/auditorRequests/AuditorRequest.java
+++ src/main/java/acme/entities/auditorRequests/AuditorRequest.java
@@ -5,6 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import acme.entities.applications.Status;
 import acme.framework.entities.Authenticated;
@@ -26,6 +27,7 @@ public class AuditorRequest extends DomainEntity {
 	@NotBlank
 	private String				responsibilityStatement;
 
+	@NotNull
 	private Status				status;
 
 	@ManyToOne(optional = false)
